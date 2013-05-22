8bfdd9ac53cb7855c6153564a72db93cc01ea3ad
Maksymilian Barnas <max.barnas@gmail.com>
2013-05-22 21:49:01 +0200

    Creating general structure of main class and testing environment

--- circular_buffer.php
+++ circular_buffer.php
@@ -1,8 +1,30 @@
 <?php
 
 /** 
-    Circular buffer implementation
-    Author: Max Barnas
-    Date: 2013.05.22
+ * Circular buffer implementation
+ * Author: Max Barnas
+ * Date: 2013.05.22
 **/
 
+class CB 
+{
+    public function __construct() 
+    {
+
+    }
+
+    public function isEmpty()
+    {
+
+    }
+
+    public function isFull()
+    {
+
+    }
+
+    public function put()
+    {
+
+    }
+}
--- test_circular_buffer.php
+++ test_circular_buffer.php
@@ -1,24 +1,45 @@
 <?php
 
 /**
-Circular buffer implementation TESTS
-**/
+ * Circular buffer implementation TESTS
+ * Author: Max Barnas
+ * Date: 2013.05.22
+ **/
+
+/** 
+ * Tests to run 
+ **/
 $tests = array(
     'testIfBufferIsEmpty'
 );
 
+require_once "circular_buffer.php";
+
+/** 
+  * Preparing tested object 
+  **/
+function init() 
+{
+    return new CB();
+}
+
+/** 
+ * Actual loop for running tests 
+ **/
 function run() {
     global $tests;
     foreach ($tests as $test) {
         if (function_exists($test)) {
-            $results[$test] = call_user_func($test);
+            $results[$test] = call_user_func($test, init());
         }
     }
 
+    echo '<pre>';
     var_dump($results);
+    echo '</pre>';
 }
 
-function testIfBufferIsEmpty()
+function testIfBufferIsEmpty($obj)
 {
     return false;
 }
